4a89e60a3a7cb352cb2fc6539a3220f60ba5732e
Ramazan Vrgn <ramazanvrgn31@gmail.com>
2025-02-04 23:39:11 +0300

    Add project files.

---
+++ DataStructers/Tree/BinarySearchTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructers.Tree.BinarySearchTree
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T>  Right { get; set; }
+
+        public Node()
+        {
+            
+        }
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+    }
+}
